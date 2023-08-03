79df9f563d6c74ed62d3777a971c099f3e4d6fbe
eduardo <marketingdoeduardo@gmail.com>
2023-08-02 22:20:17 -0300

    Atualização do jquery pro js

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-$(document).ready(function() {
-    $('.hamburguer').click(function() {
-        $('ul').slideToggle()
+let up = document.querySelector(".volta");
+let hamburguer = document.querySelector(".hamburguer").addEventListener('click', () => {
+    let aparecer = document.querySelector("#slide");
+    let estiloDisplay = window.getComputedStyle(aparecer).display;
+    if(estiloDisplay === 'none') {
+        aparecer.style.display = 'block'
+    }else {
+        aparecer.style.display = 'none';
+    }
+})
+
+up.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
     })
 })
+
+function comportamentoScroll() {
+    if (window.scrollY === 0) {
+        document.querySelector(".volta").style.display = 'none';
+    }else {
+        document.querySelector(".volta").style.display = 'block';
+    }
+}
+
+addEventListener('scroll', comportamentoScroll);
